da9c475b3c9c87a6fef4923748ffce75e59ed710
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2023-04-17 13:19:29 +0000

    Add files

--- .ccls-cache/@home@runner@cs111/assignment_2@2.c
+++ .ccls-cache/@home@runner@cs111/assignment_2@2.c
@@ -1,88 +1,158 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void generateOriginalMatrix(int u, int m, int original_matrix[u][m])
-{
-    for(int i = 0; i < u; i++)
-        for(int j = 0; j < m; j++)
-            original_matrix[i][j] = (rand() % 5) + 1;
+void generateOriginalMatrix(int u, int m, int original[u][m]) {
+  for (int i = 0; i < u; i++)
+    for (int j = 0; j < m; j++)
+      original[i][j] = (rand() % 5) + 1;
 }
 
-void removeRandomElements(int u, int m, int original_matrix[u][m],
-                          int matrix_with_missing_elements[u][m])
-{
-    /*
-        0 - Deletion
-        1 - Retention
-    */
-    for(int i = 0; i < u; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            int decision = rand()%2;
-            if(decision) matrix_with_missing_elements[i][j] = original_matrix[i][j];
-            else matrix_with_missing_elements[i][j] = 0;
-        }        
+void removeRandomElements(int u, int m, int original[u][m],
+                          int missing_elements[u][m]) {
+  /*
+      0 - Deletion
+      1 - Retention
+  */
+  for (int i = 0; i < u; i++) {
+    for (int j = 0; j < m; j++) {
+      int decision = rand() % 2;
+      if (decision)
+        missing_elements[i][j] = original[i][j];
+      else
+        missing_elements[i][j] = 0;
     }
+  }
 }
 
-void fillMatrixRandomly(int u, int m, int matrix_with_missing_elements[u][m],
-                        int matrix_filled_randomly[u][m])
-{
-    for(int i = 0; i < u; i++)
-    {
-        for(int j = 0; j < m; j++)
-        {
-            if(matrix_with_missing_elements[i][j]) 
-                matrix_filled_randomly[i][j] = matrix_with_missing_elements[i][j];
-            else 
-                matrix_filled_randomly[i][j] = (rand()%5) + 1;
-        }
+void fillMatrixRandomly(int u, int m, int missing_elements[u][m],
+                        int randomly_filled[u][m]) {
+  for (int i = 0; i < u; i++) {
+    for (int j = 0; j < m; j++) {
+      if (missing_elements[i][j])
+        randomly_filled[i][j] = missing_elements[i][j];
+      else
+        randomly_filled[i][j] = (rand() % 5) + 1;
     }
+  }
 }
 
-void fillMatrixWithRowAverage(int u, int m, int matrix_with_missing_elements[u][m],
-                              int matrix_filled_with_row_average[u][m])
-{
-    for(int i = 0; i < u; i++)
-    {
-        
+void fillMatrixWithRowAverage(int u, int m, int missing_elements[u][m],
+                              int rowavg[u][m]) {
+  for (int i = 0; i < u; i++) {
+    int n_nonzero = 0, row_avg = 0;
+    double row_sum = 0;
+    for (int j = 0; j < m; j++) {
+      if (missing_elements[i][j]) {
+        row_sum += missing_elements[i][j];
+        rowavg[i][j] = missing_elements[i][j];
+        n_nonzero++;
+      }
     }
+    if (n_nonzero)
+      row_avg = round(row_sum / n_nonzero);
+    else
+      row_avg = rand() % 5 + 1; // To fix the n_nonzero = 0 case
+    for (int j = 0; j < m; j++)
+      if (!missing_elements[i][j])
+        rowavg[i][j] = row_avg;
+  }
 }
 
-void fillMatrixWithColumnAverage(int u, int m, int matrix_with_missing_elements[u][m],
-                              int matrix_filled_with_column_average[u][m]);
-
-void reportDeviation(int u, int m, int original[u][m], int matrix[u][m], int c)
-{
-    printf("\nThe deviation in case of Q(%c) is\n", c);
-    for(int i = 0; i < u; i++)
-    {
-        for(int j = 0; j < m; j++)
-            printf("%d ", (matrix[i][j] - original[i][j]));
-        printf("\n");
+void fillMatrixWithColumnAverage(int u, int m, int missing_elements[u][m],
+                                 int columnavg[u][m]) {
+  for (int i = 0; i < m; i++) {
+    int n_nonzero = 0, column_avg = 0;
+    double column_sum = 0;
+    for (int j = 0; j < u; j++) {
+      if (missing_elements[j][i]) {
+        column_sum += missing_elements[j][i];
+        columnavg[j][i] = missing_elements[j][i];
+        n_nonzero++;
+      }
     }
+    if (n_nonzero)
+      column_avg = round(column_sum / n_nonzero);
+    else
+      column_avg = rand() % 5 + 1; // To fix the n_nonzero = 0 case
+    for (int j = 0; j < u; j++)
+      if (!missing_elements[j][i])
+        columnavg[j][i] = column_avg;
+  }
 }
 
-int main()
-{
-    int u = 0, m = 0;
-    printf("Enter the number of users.\n");
-    scanf("%d", &u);
-    printf("Enter the number of movies.\n");
-    scanf("%d", &m);
-    int original_matrix[u][m];
-    generateOriginalMatrix(u, m, original_matrix);
-    int matrix_with_missing_elements[u][m];
-    removeRandomElements(u, m, original_matrix, matrix_with_missing_elements);
-    int matrix_filled_randomly[u][m];
-    fillMatrixRandomly(u, m, matrix_with_missing_elements, matrix_filled_randomly);
-    reportDeviation(u, m, original_matrix, matrix_filled_randomly, 'a');
-    int matrix_filled_with_row_average[u][m];
-    fillMatrixWithRowAverage(u, m, matrix_with_missing_elements, matrix_filled_with_row_average);
-    reportDeviation(u, m, original_matrix, matrix_filled_with_row_average, 'b');
-    int matrix_filled_with_column_average[u][m];
-    fillMatrixWithColumnAverage(u, m, matrix_with_missing_elements, matrix_filled_with_column_average);
-    reportDeviation(u, m, original_matrix, matrix_filled_with_column_average, 'c');
-    return 0;
+void reportDeviation(int u, int m, int original[u][m], int matrix[u][m],
+                     int c) {
+  printf("\nThe deviation in case of Q(%c) is\n", c);
+  for (int i = 0; i < u; i++) {
+    for (int j = 0; j < m; j++)
+      printf("%d ", (matrix[i][j] - original[i][j]));
+    printf("\n");
+  }
+}
+
+void fillMatrixWithRowColumnAverage(int u, int m, int missing_elements[u][m],
+                                    int rowcolumnavg[u][m]) {
+  int row_avg[u], column_avg[m];
+  for (int i = 0; i < u; i++) {
+    float row_sum = 0;
+    int n_nonzero = 0;
+    for (int j = 0; j < m; j++) {
+      row_sum += missing_elements[i][j];
+      if (missing_elements[i][j])
+        n_nonzero++;
+    }
+    if (n_nonzero)
+      row_avg[i] = round(row_sum / n_nonzero);
+    else
+      row_avg[i] = rand() % 5 + 1;
+  }
+  for (int i = 0; i < m; i++) {
+    float column_sum = 0;
+    int n_nonzero = 0;
+    for (int j = 0; j < u; j++) {
+      column_sum += missing_elements[j][i];
+      if (missing_elements[j][i])
+        n_nonzero++;
+    }
+    if (n_nonzero)
+      column_avg[i] = round(column_sum / n_nonzero);
+    else
+      column_avg[i] = rand() % 5 + 1;
+  }
+  for (int i = 0; i < u; i++) {
+    for (int j = 0; j < m; j++) {
+      if (!missing_elements[i][j])
+        rowcolumnavg[i][j] = round((row_avg[i] + column_avg[j]) / 2.0);
+      else
+        rowcolumnavg[i][j] = missing_elements[i][j];
+    }
+  }
+}
+
+int main() {
+  srand(time(0));
+  int u = 0, m = 0;
+  printf("Enter the number of users.\n");
+  scanf("%d", &u);
+  printf("Enter the number of movies.\n");
+  scanf("%d", &m);
+  int original[u][m];
+  generateOriginalMatrix(u, m, original);
+  int missing_elements[u][m];
+  removeRandomElements(u, m, original, missing_elements);
+  int randomly_filled[u][m];
+  fillMatrixRandomly(u, m, missing_elements, randomly_filled);
+  reportDeviation(u, m, original, randomly_filled, 'a');
+  int rowavg[u][m];
+  fillMatrixWithRowAverage(u, m, missing_elements, rowavg);
+  reportDeviation(u, m, original, rowavg, 'b');
+  int columnavg[u][m];
+  fillMatrixWithColumnAverage(u, m, missing_elements, columnavg);
+  reportDeviation(u, m, original, columnavg, 'c');
+  int rowcolumnavg[u][m];
+  fillMatrixWithRowColumnAverage(u, m, missing_elements, rowcolumnavg);
+  reportDeviation(u, m, original, rowcolumnavg, 'd');
+  return 0;
 }
